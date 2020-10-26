d4aa50baaa9d9fb36823adbac34fad0a0b244464
Ludeo <lucas.steuer1@gmx.de>
2020-10-26 22:53:51 +0100

    added new stuff

---
+++ maniatointralism/Src/Forms/FormUserProfile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace ManiaToIntralism.Forms
+{
+    public partial class FormUserProfile : Form
+    {
+        public FormUserProfile(
+            string globalRank,
+            string totalGlobalRank,
+            string country,
+            string countryRank,
+            string totalCountryRank,
+            double avgMiss,
+            double avgAcc,
+            double points,
+            double realPoints,
+            double maxPoints,
+            double difference,
+            int hundredPlays,
+            int totalMaps,
+            double pointsTillRankup,
+            string pictureLink)
+        {
+            this.InitializeComponent();
+            this.globalRankLbl.Text = globalRank + " / " + totalGlobalRank;
+            this.countryNameLbl.Text = country + " Rank";
+            this.countryRankLbl.Text = countryRank + " / " + totalCountryRank;
+            this.avgMissLbl.Text = avgMiss.ToString();
+            this.avgAccuracyLbl.Text = avgAcc + "%";
+            this.pointsLbl.Text = points.ToString();
+            this.realPointsLbl.Text = realPoints.ToString();
+            this.maxPointsLbl.Text = maxPoints.ToString();
+            this.differenceLbl.Text = difference.ToString();
+            this.hundredLbl.Text = hundredPlays.ToString();
+            this.totalMapsLbl.Text = totalMaps.ToString();
+            this.pointsRankupLbl.Text = pointsTillRankup.ToString();
+            WebClient webClient = new WebClient();
+            Stream stream = webClient.OpenRead(pictureLink);
+            Image image = Image.FromStream(stream);
+            Image newImage = (Image)(new Bitmap(image, new Size(this.pictureLbl.Width, this.pictureLbl.Height)));
+            this.pictureLbl.Image = newImage;
+        }
+            
+    }
+}
---
+++ maniatointralism/Src/Forms/FormUserScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Windows.Forms;
+
+namespace ManiaToIntralism.Forms
+{
+    public partial class FormUserScore : Form
+    {
+        public FormUserScore(object[][] allScores)
+        {
+            //TODO not working right now
+            InitializeComponent();
+            DataTable table = new DataTable();
+            table.Columns.Add("Map Name");
+            table.Columns.Add("Score");
+            table.Columns.Add("Accuracy");
+            table.Columns.Add("Miss");
+            table.Columns.Add("My Points");
+            table.Columns.Add("Max Points");
+            table.Columns.Add("Difference");
+            table.Columns.Add("Broken?");
+
+            for (int i = 0; i < allScores.Length; i++)
+            {
+                table.Rows.Add(allScores[i]);
+            }
+            DataGridView grid = new DataGridView();
+            this.Controls.Add(grid);
+            grid.DataSource = table;
+        }
+    }
+}
