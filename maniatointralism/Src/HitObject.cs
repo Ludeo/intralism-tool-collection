aa80599576d5404bfb3754efc16e45a496508509
Ludeo <lucas.steuer1@gmx.de>
2020-11-12 23:38:01 +0100

    fixed mania convert, changed setting form to only have one save button

--- maniatointralism/Src/HitObject.cs
+++ maniatointralism/Src/HitObject.cs
@@ -1,16 +1,29 @@
-﻿using ManiaToIntralism.Enums;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ManiaToIntralism.Enums;
 
 namespace ManiaToIntralism
 {
     public readonly struct HitObject
     {
-        public Position Position { get; }
-        public double   Timing   { get; }
-    
-        public HitObject(Position pos, double timing)
+        public string Position { get; }
+        public double Timing   { get; }
+
+        public HitObject(string maniaNote, double timing)
         {
-            this.Position = pos;
+            this.Position = ParseManiaNote(maniaNote);
             this.Timing = timing;
+        }
+        
+        private static string ParseManiaNote(string s)
+        {
+            Console.WriteLine(s);
+            
+            string[] nums = s.Split("-");
+            IEnumerable<Position> positions = nums.Select(e => (Position)Enum.Parse(typeof(Position), e));
+
+            return string.Join('-', positions);
         }
     }
 }
