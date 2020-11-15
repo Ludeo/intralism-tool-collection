a9fe2299949953d59867b9d108ece9cbb7854624
FRUP <flyingrabidunicornpig@gmail.com>
2020-11-14 21:32:19 -0800

    added first draft of UI Layout

--- IntralismToolBox/Src/Forms/FormMapEditor.Designer.cs
+++ IntralismToolBox/Src/Forms/FormMapEditor.Designer.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 
 namespace ManiaToIntralism.Forms
 {
@@ -31,6 +31,7 @@ namespace ManiaToIntralism.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMapEditor));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
@@ -40,17 +41,32 @@ namespace ManiaToIntralism.Forms
             this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.redoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.eventConfig = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
             this.defaultConfig = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
             this.splitConfigs = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
             this.resizeNS = new System.Windows.Forms.Panel();
             this.resizeWE = new System.Windows.Forms.Panel();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
+            this.eventConfig.SuspendLayout();
+            this.defaultConfig.SuspendLayout();
+            this.splitConfigs.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (22)))), ((int) (((byte) (12)))), ((int) (((byte) (52)))));
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileToolStrip, this.editToolStripMenuItem});
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (36)))), ((int) (((byte) (77)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileToolStrip, this.editToolStripMenuItem, this.settingsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
@@ -104,14 +120,14 @@ namespace ManiaToIntralism.Forms
             // 
             this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
             this.undoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys) ((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
-            this.undoToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.undoToolStripMenuItem.Text = "Undo";
             // 
             // redoToolStripMenuItem
             // 
             this.redoToolStripMenuItem.Name = "redoToolStripMenuItem";
             this.redoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys) ((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Y)));
-            this.redoToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.redoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.redoToolStripMenuItem.Text = "Redo";
             // 
             // eventConfig
@@ -120,12 +136,23 @@ namespace ManiaToIntralism.Forms
             this.eventConfig.AutoSize = true;
             this.eventConfig.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (19)))), ((int) (((byte) (7)))), ((int) (((byte) (37)))));
             this.eventConfig.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.eventConfig.Controls.Add(this.label6);
             this.eventConfig.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.eventConfig.Location = new System.Drawing.Point(0, 528);
             this.eventConfig.Name = "eventConfig";
             this.eventConfig.Padding = new System.Windows.Forms.Padding(2);
-            this.eventConfig.Size = new System.Drawing.Size(1914, 233);
+            this.eventConfig.Size = new System.Drawing.Size(1184, 233);
             this.eventConfig.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label6.Location = new System.Drawing.Point(8, 6);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(1168, 25);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Context Sensitive Toolbox? (Changes options based on selected events?)";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // defaultConfig
             // 
@@ -133,6 +160,8 @@ namespace ManiaToIntralism.Forms
             this.defaultConfig.AutoSize = true;
             this.defaultConfig.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (19)))), ((int) (((byte) (7)))), ((int) (((byte) (37)))));
             this.defaultConfig.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.defaultConfig.Controls.Add(this.label1);
+            this.defaultConfig.Controls.Add(this.richTextBox4);
             this.defaultConfig.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.defaultConfig.Location = new System.Drawing.Point(832, 24);
             this.defaultConfig.Name = "defaultConfig";
@@ -140,18 +169,139 @@ namespace ManiaToIntralism.Forms
             this.defaultConfig.Size = new System.Drawing.Size(352, 498);
             this.defaultConfig.TabIndex = 2;
             // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.Location = new System.Drawing.Point(2, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 25);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Default View";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox4.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (14)))), ((int) (((byte) (5)))), ((int) (((byte) (27)))));
+            this.richTextBox4.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.richTextBox4.ForeColor = System.Drawing.Color.Gainsboro;
+            this.richTextBox4.Location = new System.Drawing.Point(5, 30);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(339, 458);
+            this.richTextBox4.TabIndex = 17;
+            this.richTextBox4.Text = "This will show the config or parts of the config in the default Intralism JSON fo" + "rmat. Will be formatted to be nicer looking obviously.";
+            // 
             // splitConfigs
             // 
             this.splitConfigs.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             this.splitConfigs.AutoSize = true;
             this.splitConfigs.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (19)))), ((int) (((byte) (7)))), ((int) (((byte) (37)))));
             this.splitConfigs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitConfigs.Controls.Add(this.label5);
+            this.splitConfigs.Controls.Add(this.label4);
+            this.splitConfigs.Controls.Add(this.label3);
+            this.splitConfigs.Controls.Add(this.label2);
+            this.splitConfigs.Controls.Add(this.richTextBox3);
+            this.splitConfigs.Controls.Add(this.richTextBox2);
+            this.splitConfigs.Controls.Add(this.richTextBox1);
+            this.splitConfigs.Controls.Add(this.richTextBox6);
             this.splitConfigs.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.splitConfigs.Location = new System.Drawing.Point(0, 24);
             this.splitConfigs.Name = "splitConfigs";
             this.splitConfigs.Padding = new System.Windows.Forms.Padding(2);
             this.splitConfigs.Size = new System.Drawing.Size(826, 498);
             this.splitConfigs.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label5.Location = new System.Drawing.Point(5, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(394, 25);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Speed Events";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label4.Location = new System.Drawing.Point(422, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(394, 25);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Zoom Events";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label3.Location = new System.Drawing.Point(422, 2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(394, 25);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Storyboard Events";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.Location = new System.Drawing.Point(5, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(394, 25);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Arc Spawns";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox3.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (14)))), ((int) (((byte) (5)))), ((int) (((byte) (27)))));
+            this.richTextBox3.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.richTextBox3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.richTextBox3.Location = new System.Drawing.Point(422, 30);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(394, 215);
+            this.richTextBox3.TabIndex = 20;
+            this.richTextBox3.Text = resources.GetString("richTextBox3.Text");
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (14)))), ((int) (((byte) (5)))), ((int) (((byte) (27)))));
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.richTextBox2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.richTextBox2.Location = new System.Drawing.Point(5, 30);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(394, 215);
+            this.richTextBox2.TabIndex = 19;
+            this.richTextBox2.Text = "These text boxes do not resize yet. Rich Text Boxes do not seem to have auto-sizi" + "ng functions, so we will need to make our own resizing.";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (14)))), ((int) (((byte) (5)))), ((int) (((byte) (27)))));
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.richTextBox1.Location = new System.Drawing.Point(422, 273);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(394, 215);
+            this.richTextBox1.TabIndex = 18;
+            this.richTextBox1.Text = "Perhaps events are listed in a friendlier format here and if the user clicks on t" + "hem they can expand to see the full details (Default View/Context-Sensitive Tool" + "box)";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox6.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (14)))), ((int) (((byte) (5)))), ((int) (((byte) (27)))));
+            this.richTextBox6.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.richTextBox6.ForeColor = System.Drawing.Color.Gainsboro;
+            this.richTextBox6.Location = new System.Drawing.Point(5, 273);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(394, 215);
+            this.richTextBox6.TabIndex = 17;
+            this.richTextBox6.Text = "Option to enable/disable specific boxes would be very nice, especially if a user " + "only wants to work on storyboard or only wants to work on zooms.";
             // 
             // resizeNS
             // 
@@ -162,9 +312,9 @@ namespace ManiaToIntralism.Forms
             this.resizeNS.Name = "resizeNS";
             this.resizeNS.Size = new System.Drawing.Size(1184, 13);
             this.resizeNS.TabIndex = 5;
-            this.resizeNS.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMouseUp);
             this.resizeNS.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMouseDown);
             this.resizeNS.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ResizeNorthAndSouth);
+            this.resizeNS.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMouseUp);
             // 
             // resizeWE
             // 
@@ -175,9 +325,16 @@ namespace ManiaToIntralism.Forms
             this.resizeWE.Name = "resizeWE";
             this.resizeWE.Size = new System.Drawing.Size(16, 507);
             this.resizeWE.TabIndex = 6;
-            this.resizeWE.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMouseUp);
             this.resizeWE.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMouseDown);
             this.resizeWE.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ResizeWestAndEast);
+            this.resizeWE.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMouseUp);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Enabled = false;
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
             // 
             // FormMapEditor
             // 
@@ -198,13 +355,34 @@ namespace ManiaToIntralism.Forms
             this.Text = "FormMapEditor";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.eventConfig.ResumeLayout(false);
+            this.defaultConfig.ResumeLayout(false);
+            this.splitConfigs.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.Panel resizeWE;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
 
         private System.Windows.Forms.Panel defaultConfig;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+
+        private System.Windows.Forms.Panel resizeWE;
+
         private System.Windows.Forms.Panel resizeNS;
         private System.Windows.Forms.Panel splitConfigs;
 
